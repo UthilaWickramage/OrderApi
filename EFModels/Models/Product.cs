e844cab0f70fb81646ca4ba2e4befcc55c2d07c8
uthil <uthil@AXCITO-DEV-01>
2024-03-19 13:00:35 +0530

    Add project files.

---
+++ EFModels/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModels.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+
+        public string ProductDescription { get; set; }
+
+        [Required]
+        public double Price {  get; set; }
+
+            
+        [Required]
+        public int Qty { get; set; }
+    }
+}
---
+++ EFModels/Models/ProductDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModels.Models
+{
+    public class ProductDBContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+
+
+
+      
+
+
+    }
+}
